130e29c19b48f1fd01f9fa715cf7081733a2035b
AbirAzim <azim35-1713@diu.edu.bd>
2023-02-21 14:19:40 +0600

    initial cokmmit 2

--- graphql/src_food/schemas/Compare.ts
+++
@@ -1,9 +0,0 @@
-import { Field, ID, ObjectType } from 'type-graphql';
-import FormatedData from './FotmatedData';
-@ObjectType()
-export default class CompareData {
-  @Field()
-  year: String;
-  @Field((type) => [FormatedData])
-  fotmatedData: [FormatedData];
-}
--- utils/AllResolvers.ts
+++
@@ -1,7 +0,0 @@
-import FoodResolver from '../graphql/src_food/resolvers/FoodResolver';
-
-export default function getAllResolvers() {
-  return [
-    FoodResolver,
-  ];
-}
